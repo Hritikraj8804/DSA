2f7236616b027b29f5c92a0138c1f4c484a13428
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2023-05-08 22:19:26 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print("hello world!")
+print("variable name should not be a number.\n variable name should not start with  a number.\n variable name should not start with  a speacial characters (except underscore_).")
+print("its a case sensetive.\n")
+a=23
+b=332.546
+c="hello world"
+print(a,b,c)
+d="hii"
+e="_hello"
+print(a+b)
+print(a*c)
+print(a*e)
